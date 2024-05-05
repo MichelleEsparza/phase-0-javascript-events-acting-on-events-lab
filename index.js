fefc3e1c6928976fcfb68f7ed68140d77b300291
Michelle Esparza <michelle18esparza@gmail.com>
2024-05-05 15:33:39 -0700

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < (window.innerWidth - dodger.clientWidth)) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+
